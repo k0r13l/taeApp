e8b6ae3f7f353920caad4b163a6b1a0f4b8ff410
k0r13l <36462249+k0r13l@users.noreply.github.com>
2022-06-16 13:48:39 -0600

    Add files via upload

---
+++ TAE/TAE/ViewController.swift
@@ -0,0 +1,132 @@
+//
+//  ViewController.swift
+//  TAE
+//
+//  Created by LABORATORIOS on 16/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var menuHide: UIBarButtonItem!
+    
+    @IBOutlet var menuShow: UIBarButtonItem!
+    @IBOutlet var menu: UITableView!
+    
+    @IBOutlet var containerView: UIView!
+    
+    @IBOutlet var swipeGesture: UISwipeGestureRecognizer!
+    
+    var menuOption = false
+    let screen = UIScreen.main.bounds
+    var home = CGAffineTransform()
+    var options: [option] = [
+        option(title: "Inicio", segue:"InicioSegue"),
+        option(title: "Mapa", segue:"MapaSegue"),
+        option(title: "Sitios Turisticos", segue:"SitiosTuristicosSegue"),
+        option(title: "Ofertas", segue:"OfertasSegue"),
+        option(title: "Videos", segue:"VideosSegue"),
+        option(title: "Creditos", segue:"CreditosSegue"),
+        option(title: "Galeria", segue:"GaleriaSegue"),
+        option(title: "Login", segue:"LoginSegue"),
+        option(title: "Carrito", segue:"CarritoSegue")
+        
+    
+    ]
+    struct option{
+        var title = String()
+        var segue = String()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menu.delegate = self
+        menu.dataSource = self
+        menu.backgroundColor = .clear
+        home = containerView.transform
+        menuHide.isEnabled = false
+        menuHide.title = ""
+        menuHide.image = UIImage(systemName: "")
+    }
+    
+    func showMenu() {
+        self.containerView.layer.cornerRadius = 40
+        let x = screen.width * -0.8
+        let originalTransform = self.containerView.transform
+        let scaledTransform = originalTransform.scaledBy(x: 0.8, y: 0.8)
+        let scaledAndTranslatedTransform = scaledTransform.translatedBy(x: x, y: 0)
+        UIView.animate(withDuration: 0.7, animations: {
+            self.containerView.transform = scaledAndTranslatedTransform
+        })
+    }
+        
+    @IBAction func hidemenus(_ sender: Any) {
+        if menuOption == true {
+            print("user is hiding menu")
+            hideMenu()
+            menuOption = false
+            menuHide.isEnabled = false
+            menuHide.image = UIImage(systemName: "")
+            menuShow.isEnabled = true
+            menuShow.image = UIImage(systemName: "line.horizontal.3")
+        }
+    }
+    @IBAction func showMenus(_ sender: Any) {
+        print("menu interaction")
+        if menuOption == false && swipeGesture.direction == .right {
+            print("user is showing menu")
+            showMenu()
+            menuOption = true
+            menuHide.isEnabled = true
+            menuHide.image = UIImage(systemName: "xmark")
+            menuShow.isEnabled = false
+            menuShow.image = UIImage(systemName: "")
+        }
+    }
+
+    func hideMenu() {
+        UIView.animate(withDuration: 0.7, animations: {
+            self.containerView.transform = self.home
+            self.containerView.layer.cornerRadius = 0
+        })
+    }
+    
+    @IBAction func showMenu(_ sender: UISwipeGestureRecognizer) {
+        print("menu interaction")
+        if menuOption == false && swipeGesture.direction == .right {
+            print("user is showing menu")
+            showMenu()
+            menuOption = true
+                    
+        }
+    }
+    @IBAction func hideMenu(_ sender: Any) {
+        if menuOption == true {
+            print("user is hiding menu")
+            hideMenu()
+            menuOption = false
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let menuAux = self.options[indexPath.row]
+        cell.textLabel?.text = menuAux.title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let row = indexPath.row
+        menu.deselectRow(at: indexPath, animated: true)
+        performSegue(withIdentifier: options[row].segue, sender: self)
+
+    }
+
+
+}
+
